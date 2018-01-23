23fe2ed2dcd4ba821f3ffca57d0cc7a30fe8d470
Volha.Shut <Volha.Shut@wolterskluwer.com>
2018-01-23 17:14:50 +0300

    added AngularPage with waitForAngular

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ exports.State = require('./framework/po/State');
 exports.Collection = require('./framework/po/Collection');
 exports.Component = require('./framework/po/Component');
 exports.Page = require('./framework/po/Page');
+exports.AngularPage = require('./framework/po/AngularPage');
 exports.tasks = require('./gulp/tasks');
