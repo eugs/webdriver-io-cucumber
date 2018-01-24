da6cdc8e9dbfbd00cdd4da622aba68fd6ac8a774
Oleksandr Halichenko <Oleksandr.Halichenko@wolterskluwer.com>
2018-01-24 14:58:25 +0300

    added credential manager

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ exports.Collection = require(path.resolve('./framework/po/Collection'));
 exports.Component = require(path.resolve('./framework/po/Component'));
 exports.Page = require(path.resolve('./framework/po/Page'));
 exports.CredentialManager = require(path.resolve('./framework/credential_manager/CredentialManager'));
-exports.TaskKiller = require(path.resolve('./framework/credential_manager/TaskKiller'));
+exports.TaskKiller = require(path.resolve('./framework/taskkiller/TaskKiller'));
+exports.Memory = require(path.resolve('./framework/memory/Memory'));
 exports.tasks = require(path.resolve('./gulp/tasks'));
