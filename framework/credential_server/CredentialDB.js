95963ae1a38bf5d191f3e88d139869335effc0ce
Volha Shut <Volha_Shut@epam.com>
2018-04-12 11:02:45 +0300

    hotfix credential service

--- framework/credential_server/CredentialDB.js
+++ framework/credential_server/CredentialDB.js
@@ -33,16 +33,16 @@ class CredentialDB {
     }
 
     /**
-     * Free credentials by username
-     * @param username - username to delete
+     * Free credentials by user
+     * @param user
      */
-    freeCredentials(username) {
-        const userIndex = this.credentials.findIndex(item => item.username === username);
+    freeCredentials(user) {
+        const userIndex = this.credentials.findIndex(item => item.user === user);
 
         if (userIndex !== -1) {
             this.credentials[userIndex].isLocked = false;
         } else {
-            throw new Error(`Username ${username} isn't found`)
+            throw new Error(`Username ${user} isn't found`)
         }
     }
 
