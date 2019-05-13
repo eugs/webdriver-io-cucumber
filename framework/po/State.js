34bff18874f0a9e055cb4739f7b907f20481c6d3
Eugen Stotik <Eugen_Stotik@epam.com>
2019-05-13 11:53:16 +0300

    Update State (auto page detectioncan be turned off)

--- framework/po/State.js
+++ framework/po/State.js
@@ -1,47 +1,75 @@
 class State {
 
-    constructor() {
-        this.pages = new Map();
+  constructor() {
+      this.pages = new Map();
+      this.currentPage = '';
+      this.autoSwitchPage = true;
+  }
+
+  /**
+   * Add page
+   * @param name
+   * @param obj
+   */
+  addPage(name, obj) {
+    if ((!name) || (!obj)) {
+      throw new Error(`Wrong params: "${name}", "${obj}" in addPage()!`);
     }
 
-    /**
-     * Add page
-     * @param name
-     * @param obj
-     */
-    addPage(name, obj) {
-        this.pages.set(name, obj);
+    if (typeof name !== 'string') {
+      throw new Error(`Please set page name as a string, got: ${name}`);
     }
 
-    /**
-     * Get page by name
-     * @param name
-     * @return {Page}
-     */
-    getPageByName(name) {
-        return this.pages.get(name);
+    this.pages.set(name, obj);
+  }
+
+  /**
+   * Get page by name
+   * @param name
+   * @return {Page}
+   */
+  getPageByName(name) {
+    if (!(this.pages.get(name))) {
+      throw new Error(`No such page: ${name} in State!`);
     }
 
-    /**
-     * Get page by URL
-     * @return {Page}
-     */
-    getPage() {
-        let url = browser.getUrl();
-        let key = null;
-        this.pages.forEach((v, k) => {
-            let re = new RegExp(v.pageUrl);
-            if (url.search(re) != -1) {
-                key = k;
-            }
-        });
+    this.currentPageKey = name;
+    return this.pages.get(name);
+  }
 
-        if (!key) {
-            throw new Error(`Page ${url} is not defined!`);
+  /**
+   * Get page by URL
+   * @return {Page}
+   */
+  getPage() {
+    if (this.autoSwitchPage) {
+      let url = browser.getUrl();
+      let key = null;
+      this.pages.forEach((v, k) => {
+        let re = new RegExp(v.pageUrl);
+        if (url.search(re) !== -1) {
+          key = k;
         }
+      });
 
-        return this.pages.get(key);
+      if (!key) {
+        throw new Error(`Page ${url} is not defined!`);
+      }
+
+      this.currentPageKey = key;
     }
+
+    return this.pages.get(this.currentPageKey);
+  }
+
+  turnAutoSwitchPage(value) {
+    if (typeof value !== 'boolean') {
+      throw new Error(`Value ${value} should be boolean!`);
+    }
+
+    this.autoSwitchPage = value;
+  }
+
 }
 
 module.exports = new State();
